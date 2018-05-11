e2884ba1860f901312a7932ae2a6a99ae8f25178
Martin Almvik <martinalmvik@gmail.com>
2018-05-11 22:37:43 +0200

    Add hello world

--- src/main/kotlin/com/almyy/example/jxdemo/DemoApplication.kt
+++ src/main/kotlin/com/almyy/example/jxdemo/DemoApplication.kt
@@ -2,10 +2,21 @@ package com.almyy.example.jxdemo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class DemoApplication
 
 fun main(args: Array<String>) {
     runApplication<DemoApplication>(*args)
+}
+
+@RestController
+class Controller {
+
+    @GetMapping("/")
+    fun hello(): String {
+        return "Hello"
+    }
 }
